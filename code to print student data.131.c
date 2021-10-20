2132b792d0e42ed628b57054d8257061204aa109
SasiPabbireddy <92348095+SasiPabbireddy@users.noreply.github.com>
2021-10-20 12:29:34 +0530

    Add files via upload

---
+++ code to print student data.131.c
@@ -0,0 +1,21 @@
+/* c program to print student data*/
+#include<stdio.h>
+void main()
+{
+	// variable declaration
+	int age;
+	float cgpa;
+	char intial;
+	
+	//intialaization
+	age=20;
+	cgpa=7.8;
+	intial='S';
+	
+	//output
+	printf("Age of student is: %d\n",age);
+	printf("Cgpa of student is: %f\n",cgpa);
+	printf("Student intial starts with: %c",intial);
+	
+	
+}
