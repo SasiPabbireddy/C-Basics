01c27e535f08d94144fe543ef66b1c90d70e62b9
SasiPabbireddy <92348095+SasiPabbireddy@users.noreply.github.com>
2021-11-02 12:07:53 +0530

    Add files via upload

---
+++ arthmeticcc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,s,x,p,d,m;
+    a=11;
+    b=2;
+    s=a+b;
+    x=a-b;
+    p=a*b;
+    d=a/b;
+    m=a%b;
+    
+    
+     printf("sum of %d and %d is : %d\n",a,b,s);
+     printf("difference of %d and %d is : %d \n",a,b,x);
+     printf("product of %d and %d is : %d \n",a,b,p);
+     printf("quotient of %d and %d is : %d \n",a,b,d);
+     printf("remainder of %d and %d is :%d ",a,b,m);
+}
