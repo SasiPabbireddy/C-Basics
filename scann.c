a9698bbcb074f976a3b06a7ce4fb50a9f8c98488
SasiPabbireddy <92348095+SasiPabbireddy@users.noreply.github.com>
2021-11-09 12:27:46 +0530

    Add files via upload

---
+++ scann.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter 1st number:",a);
+	scanf("%d",&a);
+	printf("Enter 2nd number:",b);
+	scanf("%d",&b);
+	printf("Enter 3rd number:",c);
+	scanf("%d",&c);
+	printf("number 1:%d\n",a);
+	printf("number 2:%d\n",b);
+	printf("number 3:%d",c);
+}
